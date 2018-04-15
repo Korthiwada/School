d44a7b2ad7dbfea4f10c8dc52c30273a9994144c
Korthiwada <korthiwadarahul@gmail.com>
2018-04-15 15:48:52 +0530

    Some simple changes for models

--- Code/Admin/admin.py
+++ Code/Admin/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Student,Class,Login,Results,TotalMarks,Attendance
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Class)
+admin.site.register(Login)
+admin.site.register(Results)
+admin.site.register(TotalMarks)
+admin.site.register(Attendance)
--- Code/Admin/models.py
+++ Code/Admin/models.py
@@ -6,6 +6,9 @@ class Class(models.Model):
     class_strength = models.IntegerField
     class_teacher = models.CharField(max_length=100)
 
+    def __str__(self):
+        return "Class : " + str(self.class_name)
+
 class Student(models.Model):
     name = models.CharField(max_length=100)
     cl = models.ForeignKey(Class, on_delete=models.CASCADE)
@@ -13,35 +16,47 @@ class Student(models.Model):
     email = models.CharField(max_length=100)
     address = models.CharField(max_length=1000)
 
-class Results(models.Model):
-    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
-    Language_1 = models.IntegerField
-    Language_2 = models.IntegerField
-    Maths = models.IntegerField
-    Science = models.IntegerField
-    Social = models.IntegerField
-    Total = models.IntegerField
+    def __str__(self):
+        return "Student : " + str(self.name)
+
+class TotalMarks(models.Model):
+    T_Language_1 = models.IntegerField()
+    T_Language_2 = models.IntegerField()
+    T_Maths = models.IntegerField()
+    T_Science = models.IntegerField()
+    T_Social = models.IntegerField()
+    T_Total = models.IntegerField()
     Year = models.CharField(max_length=100)
-    Exam_Type = models.IntegerField
+    Exam_Type = models.CharField(max_length=100)
+    T_cl_id = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.T_cl_id) + "\t- " + str(self.Exam_Type) + "\t- " + str(self.Year)
+
+class Results(models.Model):
+    Exam_id = models.ForeignKey(TotalMarks, on_delete=models.CASCADE)
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    Language_1 = models.IntegerField()
+    Language_2 = models.IntegerField()
+    Maths = models.IntegerField()
+    Science = models.IntegerField()
+    Social = models.IntegerField()
+    Total = models.IntegerField()
     cl_id = models.ForeignKey(Class, on_delete=models.CASCADE)
     Rank = models.IntegerField()
 
-class TotalMarks(models.Model):
-    T_Language_1 = models.IntegerField
-    T_Language_2 = models.IntegerField
-    T_Maths = models.IntegerField
-    T_Science = models.IntegerField
-    T_Social = models.IntegerField
-    T_Total = models.IntegerField
-    T_Year = models.CharField(max_length=100)
-    T_Exam_Type = models.IntegerField
-    T_cl_id = models.ForeignKey(Class, on_delete=models.CASCADE)
+    def __str__(self):
+        return "Student : " + str(self.student_id.name)
+
 
 class Attendance(models.Model):
     student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
     Year = models.CharField(max_length=100)
-    Class_Attended = models.IntegerField
-    Total_Classes = models.IntegerField
+    Class_Attended = models.IntegerField()
+    Total_Classes = models.IntegerField()
+
+    def __str__(self):
+        return "Student : " + str(self.student_id.name)
 
 class Login(models.Model):
     student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
